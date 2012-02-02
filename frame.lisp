70317483900cceb7b13c6d698742518c025e42ab
daimrod <daimrod@gmail.com>
2012-02-02 16:55:18 +0100

    initial commit

---
+++ frame.lisp
@@ -0,0 +1,14 @@
+;;;; frame.lisp
+
+(define-frame-preference
+    "web"
+    (0 t t :class "Firefox"))
+
+(define-frame-preference
+    "skype"
+    (0 t t :class "Skype"))
+
+(define-frame-preference
+    "zotero"
+    (0 t t :class "Zotero"))
+
---
+++ init.lisp
@@ -0,0 +1,31 @@
+;;;; init.lisp
+
+(in-package :stumpwm)
+
+(set-prefix-key (kbd "s-s"))
+
+(ql:quickload :swank)
+(swank-loader:init)
+
+(defcommand start-swank () ()
+  "Launch a swank server
+Use M-x slime-connect"
+  (setf stumpwm:*top-level-error-action* :break)
+  (swank:create-server :port 4005
+		       :style swank:*communication-style*
+		       :dont-close t))
+
+(defvar *stumpwm-started* nil)
+(unless *stumpwm-started*
+  (setf *stumpwm-started* t)
+  (start-swank)
+  (grename "emacs")
+  (mapc #'gnewbg
+        '("web"
+          "ssh"
+          "misc"
+          "anon"
+          "skype"
+          "zotero")))
+
+(load-module "frame")
