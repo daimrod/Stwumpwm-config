7be65c0c2132eccd8ad406120d9a23459fe8888f
daimrod <daimrod@gmail.com>
2012-02-24 18:21:34 +0100

    Set tango theme configuration.

---
+++ config-theme.lisp
@@ -0,0 +1,26 @@
+;;;; config-theme.lisp
+
+(in-package #:stumpwm)
+
+(setf *message-window-gravity* :top-center
+      *input-window-gravity* :top-center)
+
+(defparameter *theme-tango* '(#x2e3436  ; black
+                              #xa40000  ; red
+                              #x4e9a06  ; green
+                              #xc4a000  ; yellow
+                              #x204a87  ; blue
+                              #x5c3566  ; magenta
+                              #x729fcf  ; cyan
+                              #xeeeeec  ; white
+                              )
+  "See http://tango.freedesktop.org/Tango_Icon_Theme_Guidelines")
+
+(setf *colors* *theme-tango*)
+
+(set-focus-color #x3465a4)
+(set-unfocus-color #x555753)
+(set-border-color #x729fcf)
+(set-bg-color #x2e3436)
+(set-fg-color #xeeeeec)
+
--- stumpwm-init.asd
+++ stumpwm-init.asd
@@ -3,8 +3,10 @@
 (asdf:defsystem #:stumpwm-init
   :serial t
   :depends-on (#:stumpwm
+               #:stumpwm-contrib
                #:swank)
   :components ((:file "config-defs")
                (:file "config-frames")
+               (:file "config-theme")
                (:file "config-bindings")
                (:file "init")))
